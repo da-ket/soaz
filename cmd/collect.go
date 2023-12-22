d2d78118bf08925a56aba123b30c9172464c20c0
da-ket <daket.duet@gmail.com>
2023-12-22 23:03:30 +0900

    cmd: validate the number of keywords and its length

--- cmd/collect.go
+++ cmd/collect.go
@@ -8,6 +8,7 @@ package cmd
 
 import (
 	"fmt"
+	"unicode/utf8"
 
 	"github.com/spf13/cobra"
 )
@@ -16,6 +17,7 @@ import (
 var collectCmd struct {
 	platform string
 	keywords []string
+	err      error
 	command  *cobra.Command
 }
 
@@ -24,7 +26,31 @@ func init() {
 		Use:   "collect",
 		Short: "Collect the meaningful data on the web",
 		Long:  "Collect the meaningful data on the web",
+		PreRun: func(cmd *cobra.Command, args []string) {
+			const keywordsNumLimit int = 3
+			if len(collectCmd.keywords) > keywordsNumLimit {
+				collectCmd.err = fmt.Errorf("the number of keywords is limited to %d, got %d", keywordsNumLimit, len(collectCmd.keywords))
+				return
+			}
+
+			const keywordLenLimit int = 25
+			for _, k := range collectCmd.keywords {
+				if utf8.RuneCountInString(k) > keywordLenLimit {
+					collectCmd.err = fmt.Errorf("each keyword is limited to %d letters, got %d", keywordLenLimit, utf8.RuneCountInString(k))
+					return
+				}
+			}
+
+			// TODO (da-ket): Deduplicate keywords should be handled here.
+
+			collectCmd.err = nil
+		},
 		Run: func(cmd *cobra.Command, args []string) {
+			if collectCmd.err != nil {
+				fmt.Printf("Error: %v\n", collectCmd.err)
+				fmt.Println(cmd.UsageString())
+				return
+			}
 			fmt.Println(collectCmd.platform)
 			fmt.Println(collectCmd.keywords)
 		},
