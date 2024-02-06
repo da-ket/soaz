6a247d42a31b9e4261d512a0afa8c756c8223ba4
da-ket <daket.duet@gmail.com>
2024-02-06 23:17:16 +0900

    cmd: add missing license header

--- cmd/collect_test.go
+++ cmd/collect_test.go
@@ -1,3 +1,9 @@
+// Copyright 2024 da-ket.
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
 package cmd
 
 import (
